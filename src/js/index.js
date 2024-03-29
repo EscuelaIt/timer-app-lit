548d1bffb17ce9915d20e9244740266a506dc059
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2024-03-29 16:03:35 +0100

    codigo de prueba de axios

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,25 @@ import '../css/style.css';
 import './tm-app';
 import './components/tm-time-counter';
 
+import Axios from 'axios';
+
 console.log("hola");
+
+export const axios = Axios.create({
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Accept': 'application/json'
+  },
+});
+
+axios.get('https://jsonplaceholder.typicode.com/todos/6')
+  .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response);
+  })
+  .catch( error => {
+    console.log(error);
+  })
+  .finally( () => {
+    console.log('ok ha terminado');
+  })
